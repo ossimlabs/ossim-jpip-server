5728a167f18980d6d9dae4a36a3ddbd6bf640af1
Aaron Nies <aaron.nies@digitalglobe.com>
2017-10-19 13:16:09 -0400

    Added Default params to ossim-jpip-server command.

--- docker/run.sh
+++ docker/run.sh
@@ -1,14 +1,38 @@
 #!/bin/bash
 
+# Required params
+
 if [ -z ${JPIP_DATA_DIR} ]; then
  echo "JPIP_DATA_DIR environment must be defined"
  exit 1
 fi
 
+# Defaulted params
+
 if [ -z ${SOURCES} ] ; then
    export SOURCES=25
 fi
 
+if [ -z ${CLIENTS} ] ; then
+   export CLIENTS=25
+fi
+
+if [ -z ${PORT} ] ; then
+   export PORT=8080
+fi
+
+if [ -z ${MAX_RATE} ] ; then
+   export MAX_RATE=40000000
+fi
+
+if [ -z ${ADDRESS} ] ; then
+   export ADDRESS=localhost
+fi
+
+if [ -z ${CONNECTION_THREADS} ] ; then
+   export CONNECTIONS_THREADS=100
+fi
+
 pushd ${JPIP_DATA_DIR} >/dev/null
-ossim-jpip-server -sources ${SOURCES} -clients 25 -port 9000 -max_rate 40000000 -address localhost -connection_threads 100
+ossim-jpip-server -sources ${SOURCES} -clients ${CLIENTS} -port ${PORT} -max_rate ${MAX_RATE} -address ${ADDRESS} -connection_threads ${CONNECTIONS_THREADS}
 popd > /dev/null
